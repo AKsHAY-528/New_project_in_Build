7822623c4a525d84dc55f6eda41c9c14dbe9796f
AKsHAY-528 <trakshaysharma@gmail.com>
2026-04-18 10:04:16 +0530

    Application file

---
+++ app.js
@@ -0,0 +1,47 @@
+import CallStack from './src/engine/callStack.js';
+import TaskQueue from './src/engine/taskQueue.js';
+import Scheduler from './src/engine/scheduler.js';
+import EventLoop from './src/engine/eventLoop.js';
+import SimulatorController from './src/controller/simulatorController.js';
+import { basicScenario } from './src/controller/scenarioManager.js';
+import { renderAll } from './src/ui/renderer.js';
+
+const callStack = new CallStack();
+const microtaskQueue = new TaskQueue("microtask");
+const callbackQueue = new TaskQueue("callback");
+
+const scheduler = new Scheduler(microtaskQueue, callbackQueue);
+const log = [];
+
+const controller = new SimulatorController(callStack, scheduler, log);
+const eventLoop = new EventLoop(callStack, scheduler);
+
+function updateUI() {
+  renderAll({
+    stack: callStack.getAll(),
+    microtasks: microtaskQueue.getAll(),
+    callbacks: callbackQueue.getAll(),
+    logs: log
+  });
+
+  attachEvents();
+}
+
+function attachEvents() {
+  document.getElementById("run").onclick = () => {
+    basicScenario(controller);
+    updateUI();
+  };
+
+  document.getElementById("step").onclick = () => {
+    eventLoop.tick();
+    updateUI();
+  };
+
+  document.getElementById("reset").onclick = () => {
+    controller.reset();
+    updateUI();
+  };
+}
+
+updateUI();
